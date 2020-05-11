4d4f57c94df8d6ce085ce6ea61ed454cd3597bae
Yakov Manshin <git@yakovmanshin.com>
2020-05-11 23:19:41 +0300

    YMScene Target

--- Package.swift
+++ Package.swift
@@ -4,10 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "YMScene",
+    platforms: [
+        .iOS(.v8),
+    ],
     products: [
         .library(
             name: "YMScene",
             targets: ["YMScene"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "YMScene"
         ),
     ]
 )
