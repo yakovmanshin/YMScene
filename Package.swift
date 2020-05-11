de4bbd7e908660b198977b4af63571dab636ac93
Yakov Manshin <git@yakovmanshin.com>
2020-05-11 22:57:07 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "YMScene",
+    products: [
+        .library(
+            name: "YMScene",
+            targets: ["YMScene"]
+        ),
+    ]
+)
