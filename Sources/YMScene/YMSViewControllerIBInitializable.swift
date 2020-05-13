bdcc03ef9805f514dfd8a674166d8efa990c9dc9
Yakov Manshin <git@yakovmanshin.com>
2020-05-13 23:05:57 +0300

    Introduced YMSViewControllerIBInitializable

---
+++ Sources/YMScene/YMSViewControllerIBInitializable.swift
@@ -0,0 +1,26 @@
+//
+//  YMSViewControllerIBInitializable.swift
+//  YMScene
+//
+//  Created by Yakov Manshin on 5/11/20.
+//  Copyright © 2020 Yakov Manshin. All rights reserved.
+//
+
+#if canImport(UIKit)
+
+public protocol YMSViewControllerIBInitializable: YMSViewControllerProtocol {
+    
+    // MARK: Associated Types
+    
+    associatedtype DTO: YMSDTOProtocol
+    
+    // MARK: Scene Configuration
+    
+    /// Use this method to configure scenes instantiated from a NIB or storyboard.
+    ///
+    /// - Parameter input: *Required.* The input used for configuring the scene.
+    func initializeScene(with input: DTO.Input)
+    
+}
+
+#endif
